f8cf3a6e0b733b5b55664a2aeef62f57ba2f419c
# unknown <90053194+frdkss@users.noreply.github.com>
2025-07-01 00:56:03 +0300

    Update view.py
    rename

--- gui/view.py
+++ gui/view.py
@@ -5,7 +5,7 @@ from PySide6.QtGui import QFont
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("ZUP-BUH Sync")
+        self.setWindowTitle("zbsync")
         # Больший размер окна
         self.resize(600, 200)
 
@@ -23,7 +23,7 @@ class MainWindow(QMainWindow):
         self.folderLineEdit.setMinimumHeight(30)
 
         # Кнопка Browse
-        self.folderButton = QPushButton("Browse…")
+        self.folderButton = QPushButton("…")
         self.folderButton.setFont(font)
         self.folderButton.setMinimumHeight(30)
 
@@ -33,7 +33,7 @@ class MainWindow(QMainWindow):
         hl.addWidget(self.folderButton)
 
         # Кнопка Go
-        self.goButton = QPushButton("Go")
+        self.goButton = QPushButton("->")
         self.goButton.setFont(font)
         self.goButton.setMinimumHeight(40)
 
